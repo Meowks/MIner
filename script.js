dae5b55b2b95077fd2c912f47c0c3b75f829cc84
Maksim <maksim20002905@gmail.com>
2024-12-16 16:06:55 +0500

    gg

--- script.js
+++ script.js
@@ -1,53 +1,127 @@
 const gameBtn = document.getElementById("gameBtn");
-document.querySelector("#loginWrapper form").addEventListener("submit",(event)=>{
-    event.preventDefault(
-        auth()
-    )
-})
+let point = 1000;
+let USERNAME;
+let game_id;
+
+document
+  .querySelector("#loginWrapper form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    auth()
+  });
+
+[...document.getElementsByClassName("point")].forEach((elem) => {
+  elem.addEventListener("click", addPoint);
+});
+
+function addPoint(event) {
+  let target = event.target;
+  point = +target.innerHTML;
+  const activeBtn = document.querySelector(".point.active");
+  if(activeBtn){
+    activeBtn.classList.remove("active");
+  }
+
+  target.classList.add("active");
+}
+
+function startOrStopGame() {
+  if (gameBtn.innerHTML == "ИГРАТЬ") {
+    gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
+    gameBtn.style.backgroundColor = "red";
+    startGame();
+  } else {
+    gameBtn.innerHTML = "ИГРАТЬ";
+    gameBtn.style.backgroundColor = "#66a663";
+    stopGame();
+  }
+}
+
+async function startGame() {
+  console.log(point);
+  let response = await sendRequest("new_game", "POST", {
+    username: USERNAME,
+    points: point,
+  });
+  if (response.error) {
+    gameBtn.innerHTML = " ИГРАТЬ";
+    alert(response.message);
+  } else {
+    game_id = response.game_id;
+    updateUserBalans();
+    activeArea();
+  }
+}
+
+async function stopGame() {
+  let response = await sendRequest("new_game", "POST", {
+    username: USERNAME,
+    game_id,
+  });
+  if (response.error) {
+    gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
+    alert(response.message);
+  } else {
+    updateUserBalans();
+    resetField();
+  }
+}
 
 // Слушатели событий
-gameBtn.addEventListener("click", activeArea);
+gameBtn.addEventListener("click", startOrStopGame);
 
 function activeArea() {
   const field = document.getElementsByClassName("field");
-  gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
 
-  gameBtn.style.backgroundColor = "red";
+  // gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
+  // gameBtn.style.backgroundColor = "red";
 
   for (let i = 0; i < field.length; i++) {
-    setInterval(() => {
+    field[i].addEventListener("contextmenu", setFlag);
+    setTimeout(() => {
       field[i].classList.add("active");
     }, 20 * i);
   }
 }
 
-let USERNAME;
-
-async function auth(){
-  const loginWrapper = document.getElementById("loginWrapper");
-  const login = document.getElementById("login").value;
-  let response = await sendRequest("user","GET",{
-      username:login,
-  });
-  if(response.error){
-    let registration = await sendRequest("user", "POST", {username: login})
-      if (registration.error) {
-          alert(registration.message);
-        } else {
-          USERNAME = login;
-          loginWrapper.style.display = "none";
-          updateUserBalans();
-        }
-    
-
-  } else {
-      USERNAME = login
-      loginWrapper.style.display="none"
-      updateUserBalans()
-  }
-
+function setFlag(event) {
+  event.preventDefault();
+  let target = event.target;
+  target.classList.toggle("flag");
 }
 
+function resetField() {
+  const gameField = document.querySelector(".gameField");
+  gameField.innerHTML = "";
+  for (let i = 0; i < 80; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("field");
+    gameField.appendChild(cell);
+  }
+}
+resetField();
+
+async function auth() {
+  const loginWrapper = document.getElementById("loginWrapper");
+  const login = document.getElementById("login").value;
+  let response = await sendRequest("user", "GET", {
+    username: login,
+  });
+  if (response.error) {
+    let registration = await sendRequest("user", "POST", { username: login });
+    if (registration.error) {
+      alert(registration.message);
+    } else {
+      USERNAME = login;
+      loginWrapper.style.display = "none";
+      updateUserBalans();
+    }
+  } else {
+    USERNAME = login;
+    loginWrapper.style.display = "none";
+    updateUserBalans();
+  }
+}
 
 async function updateUserBalans() {
   let response = await sendRequest("user", "GET", {
@@ -55,13 +129,14 @@ async function updateUserBalans() {
   });
 
   if (response.error) {
-    //alert(response.message);
+    alert(response.message);
+    console.log(response);
+    
   } else {
     const user = document.querySelector("header span");
     user.innerHTML = `Пользователь ${response.username} с балансом ${response.balance}`;
   }
 }
-updateUserBalans()
 
 
 async function sendRequest(url, method, data) {
